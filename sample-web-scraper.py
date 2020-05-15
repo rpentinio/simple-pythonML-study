6f0664418b7eae366fc094db5632323786347d18
Reggie Pentinio <reggiepen@gmail.com>
2020-05-15 15:19:20 +0800

    [Python][CODE] Modified flow for easier understanding

--- sample-web-scraper.py
+++ sample-web-scraper.py
@@ -4,7 +4,7 @@ from contextlib import closing
 from bs4 import BeautifulSoup
 
 class Dam:
-    def __init__(self, name, date1, time1, water_level1, date2, time2, water_level2, nhwl):
+    def __init__(self, name, date1, time1, water_level1, date2, time2, water_level2, difference, nhwl):
         self.name = name
         self.date1 = date1
         self.time1 = time1
@@ -12,35 +12,41 @@ class Dam:
         self.date2 = date2
         self.time2 = time2
         self.water_level2 = water_level2
+        self.difference = difference
         self.nhwl = nhwl
 
 def process_dam_info(name, details):
     # date today
-    date1 = details[13]
+    date1 = details[13].text
     # time taken today
-    time1 = details[1]
+    time1 = details[1].text
     # water level today
-    water_level1 = details[2]
+    water_level1 = details[2].text
     # date yesterday
-    date2 = details[23]
+    date2 = details[23].text
     # time taken yesterday
-    time2 = details[14]
+    time2 = details[14].text
     # water level yesterday
-    water_level2 = details[15]
+    water_level2 = details[15].text
+    # water level difference between today ad yesterday
+    difference = float(water_level1) - float(water_level2)
     # normwal high water level
-    nhwl = details[5]
+    nhwl = details[5].text
 
-    dam = Dam(name, date1, time1, water_level1, date2, time2, water_level2, nhwl)
+    dam = Dam(name, date1, time1, water_level1, date2, time2, water_level2, difference, nhwl)
 
+    return dam
+
+def print_dam_info(dam):
     print(dam.name)
-    print(dam.date1.text, dam.time1.text)
-    print("Water Level (m):", dam.water_level1.text)
-    print(dam.date2.text, dam.time2.text)
-    print("Water Level (m):", dam.water_level2.text)
-    print("Difference:", "{:.2f}".format(float(dam.water_level1.text) - float(dam.water_level2.text)), "\n")
+    print(dam.date1, dam.time1)
+    print("Water Level (m):", dam.water_level1)
+    print(dam.date2, dam.time2)
+    print("Water Level (m):", dam.water_level2)
+    print("Difference:", "{:.2f}".format(dam.difference), "\n")
 
 r = get('http://bagong.pagasa.dost.gov.ph/flood')
-soup = BeautifulSoup(r.text)
+soup = BeautifulSoup(r.text, features="html.parser")
 table = soup.find("table", attrs={'class':'dam-table'})
 table_body = table.find("tbody")
 
@@ -61,4 +67,5 @@ for row in rows:
 
     # only process the information once all items are gathered (every 4th row)
     if ctr % 4 == 0:
-        process_dam_info(dam_name, dam_info)
+        dam_info =  process_dam_info(dam_name, dam_info)
+        print_dam_info(dam_info)
